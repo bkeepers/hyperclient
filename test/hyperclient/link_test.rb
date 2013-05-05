324b513936783ff65ce3ac38cb1482393efeba0a
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2013-05-05 19:41:39 +0200

    Use futuroscope to run API calls in the background

--- test/hyperclient/link_test.rb
+++ test/hyperclient/link_test.rb
@@ -33,7 +33,7 @@ module Hyperclient
       it 'raises if no uri variables are given' do
         link = Link.new({'href' => '/orders{?id}', 'templated' => true}, entry_point)
 
-        proc { link.resource }.must_raise MissingURITemplateVariablesException
+        proc { link.resource.inspect }.must_raise MissingURITemplateVariablesException
       end
     end
 
@@ -78,7 +78,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:get).with('/productions/1')
-        link.get
+        link.get.inspect
       end
     end
 
@@ -87,7 +87,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:run_request).with(:options, '/productions/1', nil, nil)
-        link.options
+        link.options.inspect
       end
     end
 
@@ -96,7 +96,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:head).with('/productions/1')
-        link.head
+        link.head.inspect
       end
     end
 
@@ -105,7 +105,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:delete).with('/productions/1')
-        link.delete
+        link.delete.inspect
       end
     end
 
@@ -114,7 +114,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:post).with('/productions/1', {'foo' => 'bar'})
-        link.post({'foo' => 'bar'})
+        link.post({'foo' => 'bar'}).inspect
       end
     end
 
@@ -123,7 +123,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:put).with('/productions/1', {'foo' => 'bar'})
-        link.put({'foo' => 'bar'})
+        link.put({'foo' => 'bar'}).inspect
       end
     end
 
@@ -132,7 +132,7 @@ module Hyperclient
         link = Link.new({'href' => '/productions/1'}, entry_point)
 
         entry_point.connection.expects(:patch).with('/productions/1', {'foo' => 'bar'})
-        link.patch({'foo' => 'bar'})
+        link.patch({'foo' => 'bar'}).inspect
       end
     end
 
